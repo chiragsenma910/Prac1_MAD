f140b5699b71e2af4ae7235d6c9bf2f946f6ee8b
MAD_prac_1 <senmachiragkumar@gmail.com>
2023-07-14 12:41:32 +0530

    1

---
+++ src/main/kotlin/prac1.kt
@@ -0,0 +1,11 @@
+            fun main(args:Array<String>){
+                println("hello world")
+                val int_no=7 //Int
+                val double_no=9.45 //double
+                val word_char='c' //char
+                val bool_val=true //char
+                val my_text="Chirag" //string
+                val mylong:Long = 3467436654 //long
+                val my_short:Short=-106 //short
+                val my_byte:Byte=125 //byte
+            }
