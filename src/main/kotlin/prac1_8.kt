5853f83976cac53a94da7b6e99f6334decfb04dc
chiragsenma910 <139469416+chiragsenma910@users.noreply.github.com>
2023-07-21 12:33:14 +0530

    Update prac1_8.kt

---
+++ src/main/kotlin/prac1_8.kt
@@ -0,0 +1,7 @@
+import java.util.Arrays
+fun main(args: Array<String>){
+    val array1 = intArrayOf(10,9,12,17,1)
+    println(Arrays.toString(array1))
+    val array2 = arrayOfNulls<Int>(4)
+    println(Arrays.toString(array2))
+}
